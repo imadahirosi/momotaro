823c5bf0b2dd463b70140665060e5563ef25e15b
eserval <eserver_dip_jp@yahoo.co.jp>
2021-06-20 23:24:39 +0900

    change indent

--- momotaro.c
+++ momotaro.c
@@ -3,18 +3,18 @@
 enum Status {FIREWOOD_COLLECTING, WASHING};
 
 typedef struct {
-	enum Status go_to;
+  enum Status go_to;
 } an;
 
 void was_lived(){
 }
 
 int main(){
-	long long ago;
-	an old_man, old_woman;
-	was_lived();
-	old_man.go_to = FIREWOOD_COLLECTING;
-	old_woman.go_to = WASHING;
-	return 0;
+  long long ago;
+  an old_man, old_woman;
+  was_lived();
+  old_man.go_to = FIREWOOD_COLLECTING;
+  old_woman.go_to = WASHING;
+  return 0;
 }
 
